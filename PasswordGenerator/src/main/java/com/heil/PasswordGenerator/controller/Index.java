c63322d4a95266fae0228e0632081282a8533386
Jason <jasonwell@139.com>
2019-05-06 22:29:05 +0800

    密码生成器页面控制器、页面模板构建

---
+++ PasswordGenerator/src/main/java/com/heil/PasswordGenerator/controller/Index.java
@@ -0,0 +1,30 @@
+package com.heil.PasswordGenerator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author Jason <1878566968@qq.com>
+ * @version 0.0.1 
+ * @github https://github.com/orgs/heil-coder
+ *
+ */
+@Controller
+public class IndexController {
+    
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public String index(ModelMap map) {
+        map.addAttribute("title", "密码生成器~");
+        return "index";
+    }
+
+    @ResponseBody
+    @RequestMapping("/build",method=RequestMethod.POST)
+    public String bulidPassword() {
+        return "It's building!";
+    }
+}
