814c937f042435cf54b1c542603d37fd0b8b03e9
Jason <jasonwell@139.com>
2019-12-01 11:44:18 +0800

    目录名调整

--- Timestamp/src/test/java/com/heil/timestamp/TimestampApplicationTests.java
+++ Timestamp/src/test/java/com/heil/timestamp/TimestampApplicationTests.java
@@ -1,4 +1,4 @@
-package com.heil.PasswordGenerator;
+package com.heil.Timestamp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PasswordGeneratorApplicationTests {
+public class TimestampApplicationTests {
 
 	@Test
 	public void contextLoads() {
